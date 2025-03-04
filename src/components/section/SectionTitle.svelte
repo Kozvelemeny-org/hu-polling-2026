bd5d312a93885a6cc670796451cff2817a4a2346
Mihály Hideg <hidegmisi@gmail.com>
2025-03-04 04:22:16 +0100

    add variants to section title

--- src/components/section/SectionTitle.svelte
+++ src/components/section/SectionTitle.svelte
@@ -1,10 +1,31 @@
-<h2>
+<script lang="ts">
+    export let variant = "featured" as "featured" | "big" | "medium" | "small";
+</script>
+
+<h2 class={variant}>
     <slot />
 </h2>
 
-<style>
+<style lang="scss">
     h2 {
-        font-size: 26px;
-        font-weight: 500;
+        font-weight: 400;
+        font-size: 22px;
+
+        &.featured {
+            font-size: 26px;
+            font-weight: 500;
+        }
+
+        &.big {
+            font-size: 26px;
+        }
+
+        &.medium {
+            font-size: 22px;
+        }
+
+        &.small {
+            font-size: 20px;
+        }
     }
 </style>
