81709d50cca5d090c0fcb02cacadb7c617abf662
Mihály Hideg <hidegmisi@gmail.com>
2025-11-04 12:42:19 +0100

    add inline chart layout components

---
+++ src/components/ui/InlineChart.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    
+</script>
+
+<div class="chart-card">
+    <slot />
+</div>
+
+<style lang="scss">
+    .chart-card {
+        width: 100%;
+        margin: 6px 0;
+        margin-bottom: 1rem;
+        padding: 4px 6px;
+
+        overflow: visible;
+        
+        background-color: #f9f9f9;
+        //border-top: 1px solid #f5f5f5;
+        //border-bottom: 2px solid #f5f5f5;
+        border: 2px solid #f5f5f5;
+    }
+</style>
---
+++ src/components/ui/InlineChartLabel.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let description = false;
+</script>
+
+<div class="inline-chart-label" class:description>
+    <slot />
+</div>
+
+<style lang="scss">
+.inline-chart-label {
+    font-size: 14px;
+    font-weight: 400;
+    margin-bottom: 12px;
+    font-family: "Helvetica Neue", sans-serif;
+
+    &.description {
+        font-size: 12px;
+        margin-top: 12px;
+        margin-bottom: 0;
+        color: #666;
+
+        :global(p) {
+            font-size: 12px;
+            color: #666;
+            margin-top: 12px;
+            margin-bottom: 0;
+            font-family: "Helvetica Neue", sans-serif;
+        }
+    }
+
+    :global(p) {
+        font-size: 14px;
+        margin-bottom: 12px;
+        font-family: "Helvetica Neue", sans-serif;
+    }
+}
+</style>
