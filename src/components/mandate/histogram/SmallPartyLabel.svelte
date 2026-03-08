6df755d4cc5c59a367530982dd14d8e602695fed
Mihály Hideg <hidegmisi@gmail.com>
2026-03-08 14:07:13 +0100

    implement SmallPartyLabel component for the small parties section of the mandate projection page

---
+++ src/components/mandate/histogram/SmallPartyLabel.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type { PartyData } from "$lib/types";
+
+    export let party: PartyData[keyof PartyData];
+</script>
+
+<div class="labelContainer">
+    <div class="label">{party.name}</div>
+</div>
+
+<style lang="scss">
+    .labelContainer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        .label {
+            font-size: 12px;
+            font-weight: 400;
+        }
+    }
+</style>
