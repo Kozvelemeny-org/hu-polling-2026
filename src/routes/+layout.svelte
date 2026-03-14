2663704491efdbc4419e6ab4e016803d57810049
Mihály Hideg <hidegmisi@gmail.com>
2026-03-14 18:54:31 +0100

    load favicon from static

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
     import { page } from "$app/state";
-    import { PUBLIC_DATA_BASE_URL } from "$env/static/public";
 
-    const faviconHref = (PUBLIC_DATA_BASE_URL ?? '').replace(/\/$/, '') + '/favicon.png';
     $: isEmbed = page.url.pathname.includes('/embed');
 </script>
-
-<svelte:head>
-    <link rel="icon" href={faviconHref} />
-</svelte:head>
 
 {#if isEmbed}
     <div class="embed-root">
