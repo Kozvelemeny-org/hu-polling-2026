7ba977df49695d5f37ed7d84e89e827d01c30e2d
Mihály Hideg <hidegmisi@gmail.com>
2026-03-16 15:34:38 +0100

    fix embed styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
     import { page } from "$app/state";
+    import MainLayout from "$components/MainLayout.svelte";
 
     $: isEmbed = page.url.pathname.includes("/embed");
 </script>
 
-{#await import("$components/MainLayout.svelte")}
-    <div></div>
-{:then mod}
-    <svelte:component this={mod.default} embed={isEmbed}>
-        <slot />
-    </svelte:component>
-{/await}
+<MainLayout embed={isEmbed}>
+    <slot />
+</MainLayout>
